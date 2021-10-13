835b3e8a921c8d001f496b93f5719828c92248b6
Thomams23 <thomas.schlechter@hak-kitz.ac.at>
2021-10-13 11:04:35 +0200

    Ligen Form und Anmeldungs-Form erstellt

--- FootballApp/Form1.cs
+++ FootballApp/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace FootballApp
 {
-    public partial class Form1 : Form
+    public partial class Frm_Leagues : Form
     {
-        public Form1()
+        public Frm_Leagues()
         {
             InitializeComponent();
+        }
+
+        private void btn_Bundesliga_Click(object sender, EventArgs e)
+        {
+            //verlinkt auf das Bundesliga Form
+        }
+
+        private void btn_PremierLeague_Click(object sender, EventArgs e)
+        {
+            //verlinkt auf das Premier League Form
+        }
+
+        private void btn_Ligue1_Click(object sender, EventArgs e)
+        {
+            //verlinkt auf das Ligue 1 Form
+        }
+
+        private void btn_SerieA_Click(object sender, EventArgs e)
+        {
+            //verlinkt auf das Serie A Form
+        }
+
+        private void btn_LaLiga_Click(object sender, EventArgs e)
+        {
+            //verlinkt auf das LaLiga Form
         }
     }
 }
