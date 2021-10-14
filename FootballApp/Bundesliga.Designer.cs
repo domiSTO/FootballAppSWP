5a3fcb4aa2344d905db5b4d2fc2e63fbc3215b70
Thomams23 <thomas.schlechter@hak-kitz.ac.at>
2021-10-14 09:03:30 +0200

    Einzelne Liga Forms erstellt, Liga Forms mit Buttons gelinkt und team Form design gezeichnet.

---
+++ FootballApp/Bundesliga.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace FootballApp
+{
+    partial class Bundesliga
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Position_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Logo_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Wins_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Draws_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_loses_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Goaldifference_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_ConcedeGoal_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Goal_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_LastGames_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Points_Bundesliga = new System.Windows.Forms.Label();
+            this.lbl_Games_Bundesliga = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Position_Bundesliga
+            // 
+            this.lbl_Position_Bundesliga.AutoSize = true;
+            this.lbl_Position_Bundesliga.Location = new System.Drawing.Point(12, 35);
+            this.lbl_Position_Bundesliga.Name = "lbl_Position_Bundesliga";
+            this.lbl_Position_Bundesliga.Size = new System.Drawing.Size(44, 13);
+            this.lbl_Position_Bundesliga.TabIndex = 0;
+            this.lbl_Position_Bundesliga.Text = "Position";
+            // 
+            // lbl_Logo_Bundesliga
+            // 
+            this.lbl_Logo_Bundesliga.AutoSize = true;
+            this.lbl_Logo_Bundesliga.Location = new System.Drawing.Point(71, 35);
+            this.lbl_Logo_Bundesliga.Name = "lbl_Logo_Bundesliga";
+            this.lbl_Logo_Bundesliga.Size = new System.Drawing.Size(31, 13);
+            this.lbl_Logo_Bundesliga.TabIndex = 1;
+            this.lbl_Logo_Bundesliga.Text = "Logo";
+            // 
+            // lbl_Wins_Bundesliga
+            // 
+            this.lbl_Wins_Bundesliga.AutoSize = true;
+            this.lbl_Wins_Bundesliga.Location = new System.Drawing.Point(249, 35);
+            this.lbl_Wins_Bundesliga.Name = "lbl_Wins_Bundesliga";
+            this.lbl_Wins_Bundesliga.Size = new System.Drawing.Size(34, 13);
+            this.lbl_Wins_Bundesliga.TabIndex = 2;
+            this.lbl_Wins_Bundesliga.Text = "Siege";
+            // 
+            // lbl_Draws_Bundesliga
+            // 
+            this.lbl_Draws_Bundesliga.AutoSize = true;
+            this.lbl_Draws_Bundesliga.Location = new System.Drawing.Point(313, 35);
+            this.lbl_Draws_Bundesliga.Name = "lbl_Draws_Bundesliga";
+            this.lbl_Draws_Bundesliga.Size = new System.Drawing.Size(79, 13);
+            this.lbl_Draws_Bundesliga.TabIndex = 3;
+            this.lbl_Draws_Bundesliga.Text = "Unentschieden";
+            // 
+            // lbl_loses_Bundesliga
+            // 
+            this.lbl_loses_Bundesliga.AutoSize = true;
+            this.lbl_loses_Bundesliga.Location = new System.Drawing.Point(422, 35);
+            this.lbl_loses_Bundesliga.Name = "lbl_loses_Bundesliga";
+            this.lbl_loses_Bundesliga.Size = new System.Drawing.Size(64, 13);
+            this.lbl_loses_Bundesliga.TabIndex = 4;
+            this.lbl_loses_Bundesliga.Text = "Niederlagen";
+            // 
+            // lbl_Goaldifference_Bundesliga
+            // 
+            this.lbl_Goaldifference_Bundesliga.AutoSize = true;
+            this.lbl_Goaldifference_Bundesliga.Location = new System.Drawing.Point(650, 35);
+            this.lbl_Goaldifference_Bundesliga.Name = "lbl_Goaldifference_Bundesliga";
+            this.lbl_Goaldifference_Bundesliga.Size = new System.Drawing.Size(63, 13);
+            this.lbl_Goaldifference_Bundesliga.TabIndex = 7;
+            this.lbl_Goaldifference_Bundesliga.Text = "Tordifferenz";
+            // 
+            // lbl_ConcedeGoal_Bundesliga
+            // 
+            this.lbl_ConcedeGoal_Bundesliga.AutoSize = true;
+            this.lbl_ConcedeGoal_Bundesliga.Location = new System.Drawing.Point(568, 35);
+            this.lbl_ConcedeGoal_Bundesliga.Name = "lbl_ConcedeGoal_Bundesliga";
+            this.lbl_ConcedeGoal_Bundesliga.Size = new System.Drawing.Size(57, 13);
+            this.lbl_ConcedeGoal_Bundesliga.TabIndex = 6;
+            this.lbl_ConcedeGoal_Bundesliga.Text = "Gegentore";
+            // 
+            // lbl_Goal_Bundesliga
+            // 
+            this.lbl_Goal_Bundesliga.AutoSize = true;
+            this.lbl_Goal_Bundesliga.Location = new System.Drawing.Point(504, 35);
+            this.lbl_Goal_Bundesliga.Name = "lbl_Goal_Bundesliga";
+            this.lbl_Goal_Bundesliga.Size = new System.Drawing.Size(29, 13);
+            this.lbl_Goal_Bundesliga.TabIndex = 5;
+            this.lbl_Goal_Bundesliga.Text = "Tore";
+            // 
+            // lbl_LastGames_Bundesliga
+            // 
+            this.lbl_LastGames_Bundesliga.AutoSize = true;
+            this.lbl_LastGames_Bundesliga.Location = new System.Drawing.Point(811, 35);
+            this.lbl_LastGames_Bundesliga.Name = "lbl_LastGames_Bundesliga";
+            this.lbl_LastGames_Bundesliga.Size = new System.Drawing.Size(45, 13);
+            this.lbl_LastGames_Bundesliga.TabIndex = 9;
+            this.lbl_LastGames_Bundesliga.Text = "Letzte 5";
+            // 
+            // lbl_Points_Bundesliga
+            // 
+            this.lbl_Points_Bundesliga.AutoSize = true;
+            this.lbl_Points_Bundesliga.Location = new System.Drawing.Point(752, 35);
+            this.lbl_Points_Bundesliga.Name = "lbl_Points_Bundesliga";
+            this.lbl_Points_Bundesliga.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Points_Bundesliga.TabIndex = 8;
+            this.lbl_Points_Bundesliga.Text = "Punkte";
+            // 
+            // lbl_Games_Bundesliga
+            // 
+            this.lbl_Games_Bundesliga.AutoSize = true;
+            this.lbl_Games_Bundesliga.Location = new System.Drawing.Point(195, 35);
+            this.lbl_Games_Bundesliga.Name = "lbl_Games_Bundesliga";
+            this.lbl_Games_Bundesliga.Size = new System.Drawing.Size(36, 13);
+            this.lbl_Games_Bundesliga.TabIndex = 10;
+            this.lbl_Games_Bundesliga.Text = "Spiele";
+            // 
+            // Bundesliga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 450);
+            this.Controls.Add(this.lbl_Games_Bundesliga);
+            this.Controls.Add(this.lbl_LastGames_Bundesliga);
+            this.Controls.Add(this.lbl_Points_Bundesliga);
+            this.Controls.Add(this.lbl_Goaldifference_Bundesliga);
+            this.Controls.Add(this.lbl_ConcedeGoal_Bundesliga);
+            this.Controls.Add(this.lbl_Goal_Bundesliga);
+            this.Controls.Add(this.lbl_loses_Bundesliga);
+            this.Controls.Add(this.lbl_Draws_Bundesliga);
+            this.Controls.Add(this.lbl_Wins_Bundesliga);
+            this.Controls.Add(this.lbl_Logo_Bundesliga);
+            this.Controls.Add(this.lbl_Position_Bundesliga);
+            this.Name = "Bundesliga";
+            this.Text = "Bundesliga";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Position_Bundesliga;
+        private System.Windows.Forms.Label lbl_Logo_Bundesliga;
+        private System.Windows.Forms.Label lbl_Wins_Bundesliga;
+        private System.Windows.Forms.Label lbl_Draws_Bundesliga;
+        private System.Windows.Forms.Label lbl_loses_Bundesliga;
+        private System.Windows.Forms.Label lbl_Goaldifference_Bundesliga;
+        private System.Windows.Forms.Label lbl_ConcedeGoal_Bundesliga;
+        private System.Windows.Forms.Label lbl_Goal_Bundesliga;
+        private System.Windows.Forms.Label lbl_LastGames_Bundesliga;
+        private System.Windows.Forms.Label lbl_Points_Bundesliga;
+        private System.Windows.Forms.Label lbl_Games_Bundesliga;
+    }
+}
--- FootballApp/Form1.cs
+++ FootballApp/Form1.cs
@@ -20,26 +20,46 @@ namespace FootballApp
         private void btn_Bundesliga_Click(object sender, EventArgs e)
         {
             //verlinkt auf das Bundesliga Form
+            Bundesliga bundesliga = new Bundesliga();
+            bundesliga.Show();
+            this.Hide();
+            this.Close();
         }
 
         private void btn_PremierLeague_Click(object sender, EventArgs e)
         {
             //verlinkt auf das Premier League Form
+            PremierLeague premierleague = new PremierLeague();
+            premierleague.Show();
+            this.Hide();
+            this.Close();
         }
 
         private void btn_Ligue1_Click(object sender, EventArgs e)
         {
             //verlinkt auf das Ligue 1 Form
+            Ligue1 ligue1 = new Ligue1();
+            ligue1.Show();
+            this.Hide();
+            this.Close();
         }
 
         private void btn_SerieA_Click(object sender, EventArgs e)
         {
             //verlinkt auf das Serie A Form
+            SerieA seriea = new SerieA();
+            seriea.Show();
+            this.Hide();
+            this.Close();
         }
 
         private void btn_LaLiga_Click(object sender, EventArgs e)
         {
             //verlinkt auf das LaLiga Form
+            LaLiga laliga = new LaLiga();
+            laliga.Show();
+            this.Hide();
+            this.Close();
         }
     }
 }
